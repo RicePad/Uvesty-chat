b717c7858eec4411f239b84abe2e77db0d9631f1
jonathanli <jonathanli@wiredhere.com>
2016-10-23 18:50:50 +0000

    update chat functionality

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,87 @@
+ before_action :authenticate_user!
+    before_action :set_user, only: [:edit, :update, :show]
+    before_action :require_same_user, only: [:edit, :update ]
+
+  def new
+    @user = User.new
+  end
+
+  #PAPERCLIP
+  def create
+    @user = User.new( user_params )
+    if @user.save
+      redirect_to @user, notice: "Image or video was successfully uploaded"
+      else
+      render action: 'new'
+    end
+    #STRIPE STUFF!
+    # Amount in cents
+    @amount = 500
+   # Stripe.api_key = "sk_test_LtYHOQWLaju7mdlJYldtUyUI"
+
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'Rails Stripe customer',
+      :currency    => 'usd'
+    )
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  #STRIPE STUFF END!
+  end
+
+
+  def edit
+    #using require_same_user from Private and is linked from the top two 'before_action'
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    #Showing user reviews
+    @reviews = Review.where(user_id: @user.id).order("created_at DESC")
+      #shows average user review (Not working?)
+    if @review.blank?
+      @avg_review = 0
+      else
+      @avg_review = @reviews.average(:rating).round(2)
+    end
+  end
+
+  def index
+    @users = User.paginate(:page => params[:page], :per_page => 3)
+  end
+
+private
+  def user_params
+  params.require(:user).permit(:name, :firstname, :lastname, :about, :avatar, :video, :image)
+  end
+
+  def set_user
+  @user = User.find(params[:id])
+  end
+
+#requires same user to edit their user profile.
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "you can only edit your own account"
+      redirect_to root_path
+    end
+  end
+
+end
